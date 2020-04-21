6edac538a153b913665c10e9a58cd1d6cc809825
Narumi <otanarumi930@gmail.com>
2020-04-21 08:19:39 +0000

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'root_page#home'
   get  '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :posts
 end
