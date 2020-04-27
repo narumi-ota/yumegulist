f29f144fde6c49b97f8addd3e29245e2ed6d3d0a
Narumi <otanarumi930@gmail.com>
2020-04-27 04:27:33 +0000

    modify user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   before_create :create_activation_digest
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
-  before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -69,7 +68,7 @@ class User < ApplicationRecord
       end
     end
     
-        # メールアドレスをすべて小文字にする
+    # メールアドレスをすべて小文字にする
     def downcase_email
       self.email = email.downcase
     end
