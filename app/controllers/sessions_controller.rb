3da401ebfa5301dca364b24ecc40c42ca87c3149
Narumi <otanarumi930@gmail.com>
2020-04-27 03:16:10 +0000

    modify user index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,12 +16,12 @@ class SessionsController < ApplicationController
         flash[:warning] = message
         redirect_to root_url
       end
-    　else
-      flash.now[:danger] = 'メールアドレスまたはパスワードが正しくありません'
-      redirect_to login_path
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
-
+  
   def destroy
     log_out if logged_in?
     redirect_to root_url
