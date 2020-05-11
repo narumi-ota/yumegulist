015cd2d657df9b96b1e23a9d5b50998895f2061e
Narumi <otanarumi930@gmail.com>
2020-05-11 09:29:25 +0000

    modify post feature spec

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -13,13 +13,17 @@ RSpec.feature "Posts", type: :feature do
     click_button "ログイン"
   
   expect {
-    visit new_post_path
-    fill_in "施設名", with: "Test Post"
+    click_link "新規投稿"
+    fill_in "施設名", with: "Test Place"
+    select "中央区", from: "場所"
     fill_in "感想", with: "Test Post"
     click_button "投稿"
     
     expect(page).to have_content "投稿が完了しました"
+    expect(page).to have_content "Test Place"
+    expect(page).to have_content "中央区"
     expect(page).to have_content "Test Post"
   }
+  
   end
 end
