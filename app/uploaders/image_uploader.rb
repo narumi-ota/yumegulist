13a44d9eda50172976b8e099c8f68898f0fd83f8
Narumi <otanarumi930@gmail.com>
2020-05-22 10:37:10 +0000

    add uploader file for image

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,23 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+  
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+ 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+end
