5515abe1be0872429e78f15f0fb80ff8f0273c74
Narumi <otanarumi930@gmail.com>
2020-05-22 15:35:26 +0000

    modify post layout design

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,24 @@ User.create!(name:  "Test3",
              password_confirmation: "foobar",
              activated: true,
              activated_at: Time.zone.now)
+             
+User.create!(name:  "Test4",
+             email: "test4@example.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
+             
+User.create!(name:  "Test5",
+             email: "test5@example.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
+             
+User.create!(name:  "Test6",
+             email: "test6@example.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
